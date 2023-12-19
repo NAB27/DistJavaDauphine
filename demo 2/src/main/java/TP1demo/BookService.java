91444d2a4fd80d30e634f35639a435007dcedf1e
Nabih Mochir <nabih.mochir@phelma.grenoble-inp.fr>
2023-12-19 16:49:19 +0100

    restructuring

--- demo 2/src/main/java/TP1demo/BookService.java
+++
@@ -1,9 +0,0 @@
-package TP1demo;
-
-import java.util.List;
-
-public interface BookService {
-    Book createBook(Book book);
-    List<Book> findAllBooks();
-    List<Book> findBooksByName(String name);
-}
